9f7906c29bc469afa9dc3f6cd99fd39bd9ccc299
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2025-01-28 16:04:11 -0300

    teste

---
+++ src/app/models/Alert.ts
@@ -0,0 +1,10 @@
+export interface Alert {
+    id: number;
+    image_path: string;
+    start_date: string; 
+    end_date: string;
+    created_at?: string; 
+    updated_at?: string; 
+    deleted_at?: string | null; 
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,21 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import HomeScreen from "@/app/components/homeScreen/homeScreen";
 import LatestProperties from "@/app/components/latestProperties/latestProperties";
+import { Alert } from "./models/Alert";
 
 export default function Home() {
-  const [alert, setAlert] = useState(null); // Estado para armazenar o alerta
-  const [showModal, setShowModal] = useState(false); // Estado para exibir o modal
+  const [alert, setAlert] = useState<Alert | null>(null); 
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    // Faz a requisição à API ao carregar a página
     async function fetchAlert() {
       try {
         const response = await fetch(`http://localhost:8000/api/alerts/all`);
         const data = await response.json();
 
-        // Verifica se há alertas ativos e pega o primeiro
         if (data.success && data.data.activeAlerts.length > 0) {
-          setAlert(data.data.activeAlerts[0]); // Define o alerta
-          setShowModal(true); // Exibe o modal
+          setAlert(data.data.activeAlerts[0]);
+          setShowModal(true);
         }
       } catch (error) {
         console.error("Erro ao buscar alertas:", error);
@@ -34,7 +33,6 @@ export default function Home() {
       <HomeScreen />
       <LatestProperties />
 
-      {/* Exibe o modal se houver um alerta */}
       {showModal && alert && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg w-full">
@@ -46,14 +44,10 @@ export default function Home() {
             </button>
             <h2 className="text-xl font-bold mb-4 text-center">Alerta Ativo</h2>
             <img
-              src={alert.image_path} // Exibe a imagem do alerta
+              src={alert.image_path} 
               alt="Alerta Ativo"
               className="w-full h-64 object-cover rounded-lg mb-4"
             />
-            <p className="text-center">
-              <strong>De:</strong> {alert.start_date} <br />
-              <strong>Até:</strong> {alert.end_date}
-            </p>
           </div>
         </div>
       )}
